e96f49daa96860c1663dc45b27b2c0a30abb6903
ivankwongtszfung <ivankwongtszfung@gmail.com>
2018-04-22 15:01:36 +0800

    unity commit

--- scripts/GazeData.cs
+++ scripts/GazeData.cs
@@ -20,6 +20,7 @@ public class GazeData : MonoBehaviour
 
 
     }
+    
     public class MyGaze
     {
         public string status;
@@ -30,16 +31,38 @@ public class GazeData : MonoBehaviour
         }
     }
 
+    public static GazeObject getGazeData(int id)
+    {
+        return gazeList.gaze[id];
+    }
+
+    public void uploadGazeData(int id)
+    {
+        Dictionary<string, string> post = new Dictionary<string, string>() {
+                {"1",JsonUtility.ToJson(gazeList.gaze[0]).ToString()}
+            };
+        Debug.Log(post["1"]);
+        myApi.POST("http://ec2-52-15-84-235.us-east-2.compute.amazonaws.com/myFYP/public/api/setFixationData", post);
+        StartCoroutine(postRequesting());
+    }
+
     httpRequest myApi;
-    MyGaze gazeList;
+    static MyGaze gazeList;
     bool ready = false;
 
-    private IEnumerator Requesting()
+    private IEnumerator getRequesting()
     {
         yield return myApi.httpResponse;
         //Debug.Log(myApi.getResult());
         gazeList = MyGaze.CreateFromJSON(myApi.getResult());
         Debug.Log(gazeList.status);
+
+    }
+
+    private IEnumerator postRequesting()
+    {
+        yield return myApi.httpResponse;
+        Debug.Log(myApi.getResult());
 
     }
     // Use this for initialization
@@ -49,22 +72,7 @@ public class GazeData : MonoBehaviour
         gameObject.AddComponent<httpRequest>();
         myApi = gameObject.GetComponent<httpRequest>();
         myApi.GET("http://ec2-52-15-84-235.us-east-2.compute.amazonaws.com/myFYP/public/api/getFixationData");
-        StartCoroutine(Requesting());
+        StartCoroutine(getRequesting());
     }
-    void Update()
-    {
-        Debug.Log(myApi.Status());
-        if (gazeList.status == "success" && !ready)
-        {
-            Debug.Log(gazeList.gaze[0].pid);
-            Debug.Log(JsonUtility.ToJson(gazeList.gaze[0]).ToString());
-            Dictionary<string, string> post = new Dictionary<string, string>() {
-                {"1",JsonUtility.ToJson(gazeList.gaze[0]).ToString()}
-            };
-            Debug.Log(post["1"]);
-            myApi.POST("http://ec2-52-15-84-235.us-east-2.compute.amazonaws.com/myFYP/public/api/setFixationData",post);
-            StartCoroutine(Requesting());
-            ready = true;
-        }
-    }
+
 }
